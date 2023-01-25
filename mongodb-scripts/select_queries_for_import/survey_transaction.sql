ca883beb6592c314cf758a35d0f586e04dc0baa6
dasha1994 <kozy.dasha@gmail.com>
2023-01-25 10:22:25 +0200

    mongo-db: added queries for import unit, surveyTxn

--- mongodb-scripts/select_queries_for_import/survey_transaction.sql
+++ mongodb-scripts/select_queries_for_import/survey_transaction.sql
@@ -1,3 +1,12 @@
+-- txn_details table
+SELECT t.txn_id as id, t.unit_id as unitId, u.title as unitName, t.survey_id as surveyId, t.survey_title as surveyName,
+t.chrono_order_tag as chronoOrderTagId, s.session_linked as sessionLinked, s.date_created as dateCreated,
+t.date_submitted as dateSubmitted, t.suspicious_txn as isSuspicious
+ FROM txn_details t join unit u on t.unit_id = u.id
+ join survey_transaction s on t.txn_id = s.id
+ where t.unit_id = 598 and t.txn_id = 293115;
+
+-- fields
 SET @@group_concat_max_len = 2048;
 SET @sql = NULL;
 SELECT
@@ -60,3 +69,12 @@ SELECT survey_transaction_id as id, SUM(
   AS SSS_AVG,SUM(
   CASE WHEN name = "TS-REL" THEN value ELSE 0 END)
   AS TS_REL FROM survey_transaction_fields GROUP BY survey_transaction_id
+
+-- responses
+SELECT survey_transaction_id as id,
+JSON_ARRAYAGG(JSON_OBJECT('questionId',  qt.question_id, 'questionText', q.text, 'responseScore', r.score, 'response', r.text)) as responses
+from
+question_transaction qt join question q on qt.question_id = q.id
+join response_option r on qt.response = r.id
+where survey_transaction_id in (293115, 389192)
+group by survey_transaction_id;
---
+++ mongodb-scripts/select_queries_for_import/unit.sql
@@ -0,0 +1,1 @@
+select id, title as name, parent_id as parent, system_setup_id as systemSetup, unit_type_id as `type`, subtype as subtype from unit;
